18f567a3480f53665b9770455484b3e1242cd340
Tim Hansell <hansellt@gmail.com>
2018-12-31 13:44:28 -0500

    Adding tests to check the start and end time

--- babysitter.py
+++ babysitter.py
@@ -1,3 +1,16 @@
+
+def convertTime( time ):
+    if time.endswith('pm'):
+        return int(time[0:time.find('pm')])
+    elif time.endswith('am'):
+        return int(time[0:time.find('am')]) + 12
+    else:
+        return -1
+
 def babysitter( family, starttime, endtime ):
-    
-    return('fail')
+    if convertTime(starttime) <  5 or convertTime(starttime) > 15:
+        return 'Start Time is invalid'
+    elif convertTime(endtime) > 16 or convertTime(endtime) < 6:
+        return 'End Time is invalid'
+    else:
+        return 'fail'
--- test_babysitter.py
+++ test_babysitter.py
@@ -2,9 +2,18 @@ import unittest
 from babysitter import *
 
 class BabySitterTests( unittest.TestCase ):
-    
-    def test_babysitter( self ):
-        self.assertEqual( babysitter('A', '4pm', '5pm' ), 'Start Time too early' ); 
+
+    def test_convertTimeReturnsNumber(self):
+        self.assertEqual( 5, convertTime('5pm'))
+        self.assertEqual( 16, convertTime('4am'))
+
+    def test_babysitterStartTimeIsInvalid(self):
+        self.assertEqual( 'Start Time is invalid', babysitter('A', '4pm', '7pm'))
+        self.assertEqual( 'Start Time is invalid', babysitter('A', '5am', '7am'))
+
+    def test_babysitterEndTimeIsInvalid(self):
+        self.assertEqual( 'End Time is invalid', babysitter('A', '5pm', '5am'))
+        self.assertEqual( 'End Time is invalid', babysitter('A', '6pm', '5pm'))
 
 if __name__ == '__main__':
     unittest.main()
